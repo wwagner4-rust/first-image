75972e4d1c6f996387fc9df393f875822444be95
Wolfgang Wagner <wwagner4@gmail.com>
2022-12-13 15:03:56 +0100

    can create an image buffer and save it to a file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use image::{ImageBuffer, RgbImage};
+
 fn main() {
-    println!("Hello, world!");
+    let img: RgbImage = ImageBuffer::new(512, 512);
+    println!("Created an image");
+    let (width, height) = img.dimensions();
+    println!("Image width and height are {width} {height}");
+
+    let f_name = "/home/itsv.org.sv-services.at/31100428/work/rust/out/image.png";
+    image::save_buffer(f_name, &img, width, height, image::ColorType::Rgb8).unwrap();
+    println!("Wrote image to {f_name}");
+
 }
