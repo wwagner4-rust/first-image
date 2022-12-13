2a3794b8bdc47efe401ecd5e0bd95f43fd9b8ce7
Wolfgang Wagner <wwagner4@gmail.com>
2022-12-13 15:30:15 +0100

    can create a colored image

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,16 @@
 use image::{ImageBuffer, RgbImage};
 
 fn main() {
-    let img: RgbImage = ImageBuffer::new(512, 512);
-    println!("Created an image");
+    let mut img: RgbImage = ImageBuffer::new(3000, 2000);
     let (width, height) = img.dimensions();
-    println!("Image width and height are {width} {height}");
+    let mut cnt = 0;
+    let max = (width * height) as f32;
+    let _diff = 1.0 / max;
+    for p in img.pixels_mut() {
+        let color = cnt as f32 * _diff;
+        *p = image::Rgb([0, 100, (color * 256.0) as u8]);
+        cnt = cnt + 1;
+    }
 
     let f_name = "/home/itsv.org.sv-services.at/31100428/work/rust/out/image.png";
     image::save_buffer(f_name, &img, width, height, image::ColorType::Rgb8).unwrap();
