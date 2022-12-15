1c258f5bc89ca0444c09d989f58c31fd0235cf13
wwagner4 <wwagner4@gmail.com>
2022-12-15 08:37:41 +0100

    parallel processing

---
+++ src/info.md
@@ -0,0 +1,3 @@
+Create a video using:
+
+`ffmpeg -framerate 2 -y -pattern_type glob -i '*.png' out.gif`
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,19 @@
 use image::{ImageBuffer, RgbImage};
 use rand::Rng;
 use std::cmp::min;
+use rayon::prelude::*;
 
 fn main() {
-    let mut img: RgbImage = ImageBuffer::new(1000, 800);
+    let n = 10;
+    let index_list: Vec<i16> = (0..n).into_iter().collect();
+    index_list.par_iter().for_each(|i| create_and_write_image(i, &(n - 1)));
+}
+
+fn create_and_write_image(index: &i16, max_index: &i16) {
+    println!("running {index} {max_index}");
+    let _green = ((*index as f32 / *max_index as f32) * 255.0) as u8;
+    println!("blue: {:4}", _green);
+    let mut img: RgbImage = ImageBuffer::new(500, 500);
     let (width, height) = img.dimensions();
     let mut cnt = 0;
     let max = (width * height) as f32;
@@ -13,11 +23,11 @@ fn main() {
         let noise = rand::thread_rng().gen_range(0..5) as u16;
         let blue = min(((color * 256.0) as u16) + noise, 255) as u8;
         let red = (noise + 100) as u8;
-        *p = image::Rgb([red, 0, blue]);
+        *p = image::Rgb([red, _green, blue]);
         cnt = cnt + 1;
     }
-    println!("Wrote buffer");
-    let f_name = "/home/wwagner4/work/rust/out/image.png";
-    image::save_buffer(f_name, &img, width, height, image::ColorType::Rgb8).unwrap();
-    println!("Wrote image to {f_name}");
+    println!("created buffer {index}");
+    let f_name = format!("/home/wwagner4/work/rust/out/image-{:04}.png", index);
+    image::save_buffer(&f_name, &img, width, height, image::ColorType::Rgb8).unwrap();
+    println!("wrote image to {f_name}");
 }
